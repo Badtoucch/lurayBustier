ff490d5c26e81f7eb9e963f7c754c104c6e089d4
nikolay-petrenko <n.petrenko@everad.com>
2019-09-11 11:46:56 +0300

    Вносит адаптив для попапа

--- app/components/modal/modal.js
+++ app/components/modal/modal.js
@@ -8,14 +8,31 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 4000,
     cssEase: 'ease-in',
-    slidesToShow: 1
+    slidesToShow: 1,
+    responsive: [{
+      breakpoint: 640,
+      settings: 'unslick'
+    }]
+  };
+
+  var fillCarouselNavigation = function () {
+    $('.ever-popup .modal__slider-item img').each(function (index) {
+      $('.ever-popup .slick-dots li')
+          .eq(index)
+          .find('button')
+          .append('<img src="' + this.src + '" alt="" >');
+    });
   };
 
   $slider.slick(sliderSettings);
-  $('.ever-popup .modal__slider-item img').each(function (index) {
-    $('.ever-popup .slick-dots li')
-        .eq(index)
-        .find('button')
-        .append('<img src="' + this.src + '" alt="" >');
+  fillCarouselNavigation();
+
+  $(window).resize(function () {
+    if ($slider.hasClass('slick-initialized')) {
+      return;
+    }
+
+    $slider.slick(sliderSettings);
+    fillCarouselNavigation();
   });
 });
