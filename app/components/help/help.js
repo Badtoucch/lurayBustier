134985b5aa1bce4c5b4384d454ad2803defbe082
Vladislad T <v.trundenko@everad.com>
2019-09-11 17:05:55 +0300

    help-section

---
+++ app/components/help/help.js
@@ -0,0 +1,9 @@
+$(document).on("click", ".help-box__title", function(e) {
+  e.preventDefault();
+  $(this).toggleClass("active");
+  $(this)
+    .parent()
+    .find(".help-box__text")
+    .first()
+    .slideToggle(300);
+});
