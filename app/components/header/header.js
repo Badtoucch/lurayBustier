b8ac988166a7b4d8a49bef545b4fc509654a6efa
Vladislad T <v.trundenko@everad.com>
2019-09-17 17:17:34 +0300

    finish review

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,22 +1,35 @@
-$(function () {
-  var setBodyPadding = function () {
-    if ($('.header').is(':visible') && $(window).outerWidth() > 479) {
-      $(document.body).css('padding-top', $('.header').outerHeight() + 'px');
-    } else {
-      $(document.body).css('padding-top', 0);
-    }
-  };
+$(function() {
+  // var setBodyPadding = function() {
+  //   if ($(".header").is(":visible") && $(window).outerWidth() > 479) {
+  //     $(document.body).css("padding-top", $(".header").outerHeight() + "px");
+  //   } else {
+  //     $(document.body).css("padding-top", 0);
+  //   }
+  // };
 
-  setBodyPadding();
+  // setBodyPadding();
 
-  $(window).resize(function () {
-    setBodyPadding();
-  });
+  // $(window).resize(function() {
+  //   setBodyPadding();
+  // });
 
-  $(document).on('click', '.nav__link[href^="#"]', function (evt) {
+  function addMargin() {
+    let header = document.querySelector(".header");
+    let height = header.offsetHeight;
+    let nav = document.querySelector("body");
+    nav.style.marginTop = `${height}px`;
+  }
+  addMargin();
+  window.addEventListener("resize", addMargin);
+
+  $(document).on("click", '.nav__link[href^="#"]', function(evt) {
     evt.preventDefault();
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - $('.header').outerHeight() - 40
-    }, 500);
+    $("html, body").animate(
+      {
+        scrollTop:
+          $($.attr(this, "href")).offset().top - $(".header").outerHeight() - 50
+      },
+      500
+    );
   });
-})
+});
--- app/components/help/help.js
+++ app/components/help/help.js
@@ -1,12 +1,14 @@
-$(function () {
-  $('.help-box__title').click(function (evt){
+$(function() {
+  $(".help-box__title").click(function(evt) {
     evt.preventDefault();
     var $this = $(this);
-    var $parent = $this.parents('.help__box');
-
-    $parent.siblings().removeClass('active');
-    $parent.toggleClass('active');
-    $parent.siblings().find('.help-box__text').slideUp(300);
+    var $parent = $this.parents(".help__box");
+    $parent.siblings().removeClass("active");
+    $parent.toggleClass("active");
+    $parent
+      .siblings()
+      .find(".help-box__text")
+      .slideUp(300);
     $this.next().slideToggle(300);
-  })
+  });
 });
