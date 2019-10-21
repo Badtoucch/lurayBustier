dbfcf8c683d273201652c476f9283b6b9903cbb1
Vladislad T <v.trundenko@everad.com>
2019-10-21 17:19:20 +0300

    first

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,7 @@
-"use strict";
 
-//=include ../components/helpers/helpers.js
-//=include ../components/button/button.js
+
+
 //=include ../components/header/header.js
-//=include ../components/feedbacks/feedbacks.js
-//=include ../components/help/help.js
-//=include ../components/review/review.js
-//=include ../components/faq/faq.js
 //=include ../components/footer/footer.js
-//=include ../components/modal/modal.js
+
 
